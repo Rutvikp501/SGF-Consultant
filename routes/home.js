66a3171bf8f2f3f8833053540ab71f7ac8f67d90
Gainn <it@gainn.co.in>
2024-11-12 11:14:39 +0530

    test

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req,res) => {
-	res.render("home/welcome");
+	res.render("auth/login");
 });
 
 router.get("/home/about-us", (req,res) => {
